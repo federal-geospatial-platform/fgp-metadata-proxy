0cd1c91e889e2f66393fe144af9a3cae2f937104
Kevin Ballantyne <kevin.ballantyne@canada.ca>
2019-04-09 11:08:53 -0400

    modified:   scripts/pycsw-2.2.0.zip 	modified:   scripts/pycsw-2.2.0/default.cfg 	modified:   scripts/pycsw-2.2.0/pycsw/core/repository.py 	modified:   scripts/pycsw-2.2.0/pycsw/ogc/csw/csw2.py 	modified:   scripts/pycsw-2.2.0/pycsw/ogc/csw/csw3.py 	modified:   scripts/pycsw-2.2.0/pycsw/plugins/profiles/apiso/apiso.py 	modified:   scripts/pycsw-2.2.0/pycsw/server.py 	modified:   scripts/pycsw-2.2.0/run_wsgi.bat 	new file:   scripts/pycsw-2.2.0/var/log/pycsw.log

--- scripts/pycsw-2.2.0/run_wsgi.bat
+++ scripts/pycsw-2.2.0/run_wsgi.bat
@@ -1,6 +1,5 @@
-rem SET "PATH=%PATH%;C:\Python34;C:\Python34\Scripts;C:\gdal_232;C:\gdal_232\bin;C:\gdal_232\bin\gdal\apps"
-
-echo %GEOS_LIBRARY_PATH%
+set GEOS_LIBRARY_PATH=C:\pycsw-2.2.0\geos-3.3.3\src\geos.dll
+set GEOS_CONFIG=C:\pycsw-2.2.0\geos-3.3.3\src
 
 cd C:\pycsw-2.2.0
 
