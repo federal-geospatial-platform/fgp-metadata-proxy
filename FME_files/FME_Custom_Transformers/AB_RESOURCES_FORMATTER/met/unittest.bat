dfe3da00658f63e8f7075fa299e1b0211095637b
robbennett62 <robbennett@rocketmail.com>
2020-03-13 18:15:53 -0400

    Updated Unit Test AB_RESOURCES_FORMATTER

--- FME_files/FME_Custom_Transformers/AB_RESOURCES_FORMATTER/met/unittest.bat
+++ FME_files/FME_Custom_Transformers/AB_RESOURCES_FORMATTER/met/unittest.bat
@@ -16,6 +16,8 @@ REM ===========================================================================
 SET Repertoire=%~dp0
 PUSHD %Repertoire%\..
 
+
+
 REM Define FME transformer path
 SET FME_USER_RESOURCE_DIR=%USERPROFILE%\Documents\FME
 
@@ -24,6 +26,7 @@ REM Create file name variable in relative mode.
 REM ===========================================================================
 SET NomApp=AB_RESOURCES_FORMATTER
 SET fme=%FME2019%
+
 
 SET UserProfileFmx="%FME_USER_RESOURCE_DIR%\Transformers\%NomApp%.fmx"
 
@@ -40,7 +43,38 @@ SET Statut=%Statut%%ERRORLEVEL%
 
 REM Define sources
 
-@IF [%Statut%] EQU [00] (
+
+REM First FME call, source data with nine wms resources (one with error), seven esri rest resources (one with error) and seven data sets with neither resource.  
+set test_number=1
+SET source=met\source%test_number%.ffs
+set etalon=met\etalon%test_number%.ffs
+set resultat=met\resultat.ffs
+set lookup=met\ESRI_AB.csv
+set log=met\log_%test_number%.log
+set log_comp=met\log_comp_%test_number%.log
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+%fme% met\metrique_ab_resources_formatter.fmw ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+FIND "Unable to perform getCapabilites request: https://maps.alberta.ca/genesis/services/TOTO/Latest/MapServer/WMSServer?" %log%
+SET Statut=%Statut%%ERRORLEVEL%
+FIND "Unable to perform getCapabilites request: https://maps.alberta.ca/TOTO/rest/services/Land_Use_Management/Latest/MapServer?f=pjson" %log%
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+@IF [%Statut%] EQU [000000] (
  @ECHO INFORMATION : Metric test passed
  @COLOR A0
  @SET CodeSortie=999999
