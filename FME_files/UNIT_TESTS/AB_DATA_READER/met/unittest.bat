ec634576c2f7f3291553bc1bb8ff6e90cb083fc7
robbennett62 <robbennett@rocketmail.com>
2020-10-30 17:13:12 -0400

    AB_DATA_READER unit test and transformer updates

--- FME_files/UNIT_TESTS/AB_DATA_READER/met/unittest.bat
+++ FME_files/UNIT_TESTS/AB_DATA_READER/met/unittest.bat
@@ -19,7 +19,7 @@ PUSHD %Repertoire%\..
 
 
 REM Define FME transformer path
-SET FME_USER_RESOURCE_DIR=%USERPROFILE%\Documents\FME
+SET FME_USER_RESOURCE_DIR=%USERPROFILE%\Documents
 
 REM ===========================================================================
 REM Create file name variable in relative mode.
@@ -28,7 +28,8 @@ SET NomApp=AB_DATA_READER
 SET fme=%FME2019%
 
 
-SET UserProfileFmx="%FME_USER_RESOURCE_DIR%\Transformers\%NomApp%.fmx"
+SET UserProfileFmx="%FME_USER_RESOURCE_DIR%\FME\Transformers\%NomApp%.fmx"
+SET UserProfileFmxGitHub="%FME_USER_RESOURCE_DIR%\GitHub\fgp-metadata-proxy\FME_files\FME_Custom_Transformers\%NomApp%.fmx"
 
 REM ===========================================================================
 REM Initialization of the variable that contains the result of the execution
@@ -51,15 +52,18 @@ set etalon=met\etalon%test_number%.ffs
 set resultat=met\resultat.ffs
 set log=met\log_%test_number%.log
 set log_comp=met\log_comp_%test_number%.log
-set test_url=https://geodiscover.alberta.ca/geoportal/rest/metadata/item/@Value(id)/xml
+REM set test_url=https://geodiscover.alberta.ca/geoportal/rest/metadata/item/@Value(id)/xml
 
 IF EXIST %log% del %log%
 IF EXIST met\resultat.ffs DEL met\resultat.ffs
 %fme% met\metrique_ab_data_reader.fmw ^
 --IN_FFS_FILE %source% ^
 --OUT_FFS_FILE %resultat% ^
---LOG_FILE %log% ^
---GEODISCOVER_URL %test_url%
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+FIND "Data without Open Government licence" %log%
+SET Statut=%Statut%%ERRORLEVEL%
+FIND "Feature without resources" %log%
 SET Statut=%Statut%%ERRORLEVEL%
 
 REM Comparison with the standard
@@ -69,34 +73,11 @@ IF EXIST %log_comp% del %log_comp%
 --IN_RESULTAT_FILE %resultat% ^
 --LOG_FILE %log_comp% 
 SET Statut=%Statut%%ERRORLEVEL%
-FIND "Data without Open Government licence" %log%
-SET Statut=%Statut%%ERRORLEVEL%
-FIND "ERROR  converting metadata to HNAP" %log%
-SET Statut=%Statut%%ERRORLEVEL%
-FIND "Feature without resources" %log%
+
+COPY/Y %UserProfileFmxGitHub% %UserProfileFmx%
 SET Statut=%Statut%%ERRORLEVEL%
 
-REM Second FME call.  Source data for testing is an FFS file containing the preliminary query results performed by catalogue reader transformer.  
-REM An error in the Query url is added to throw error
-set test_number=1
-SET source=met\source%test_number%.ffs
-set etalon=met\etalon%test_number%.ffs
-set resultat=met\resultat.ffs
-set log=met\log_%test_number%.log
-set log_comp=met\log_comp_%test_number%.log
-set test_url=http://httpstat.us/500
-
-IF EXIST %log% del %log%
-IF EXIST met\resultat.ffs DEL met\resultat.ffs
-%fme% met\metrique_ab_data_reader.fmw ^
---IN_FFS_FILE %source% ^
---OUT_FFS_FILE %resultat% ^
---LOG_FILE %log% ^
---GEODISCOVER_URL %test_url%
-FIND "ERROR 500.  Unable to call http://httpstat.us/500" %log%
-SET Statut=%Statut%%ERRORLEVEL%
-
-@IF [%Statut%] EQU [00000000] (
+@IF [%Statut%] EQU [0000000] (
  @ECHO INFORMATION : Metric test passed
  @COLOR A0
  @SET CodeSortie=999999
