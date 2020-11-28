d7bd5333d5f87ae801cadcabd9155f1248edb23e
awoestreicher <Awoestreicher@gmail.com>
2020-11-28 02:30:34 -0500

    Unit Test for NB_ORGANIZATION_MAPPER

---
+++ FME_files/UNIT_TESTS/NB_ORGANIZATION_MAPPER/met/unittest.bat
@@ -0,0 +1,180 @@
+REM TEST 1: 9 compliant records
+REM TEST 2: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - FEATURE OUTPUT
+REM TEST 3: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - No Matching Organization Error OUTPUT
+REM Test 4: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - Missing Organization Value Error OUTPUT
+REM ===========================================================================
+REM Enable local variables 
+REM ===========================================================================
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+ 
+REM ===========================================================================
+REM Allow accented characters
+REM ===========================================================================
+chcp 1252
+
+REM ===========================================================================
+REM Determine the directory where the.bat is located and place it  
+REM in the directory above while keeping the original directory
+REM ===========================================================================
+SET Repertoire=%~dp0
+PUSHD %Repertoire%\..
+
+REM Define FME transformer path
+SET FME_USER_RESOURCE_DIR=%USERPROFILE%\Documents\FME
+
+REM ===========================================================================
+REM Create file name variable in relative mode.
+REM ===========================================================================
+SET NomApp=NB_ORGANIZATION_MAPPER
+SET fme=%FME2019%
+
+
+SET UserProfileFmx="%FME_USER_RESOURCE_DIR%\Transformers\%NomApp%.fmx"
+
+REM ===========================================================================
+REM Initialization of the variable that contains the result of the execution
+REM ===========================================================================
+SET Statut=0
+
+REM ===========================================================================
+REM Copy FMX to Documents
+REM ===========================================================================
+COPY/Y fme\%NomApp%.fmx %UserProfileFmx%
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Define sources
+
+REM First FME call,creating FFS File with nine compliant data records, feature output
+set test_number=1
+SET source=met\source%test_number%.ffs
+set etalon=met\etalon%test_number%.ffs
+set lookup=met\OrganizationLookupTable.csv
+set resultat=met\resultat.ffs
+set log=met\log_%test_number%.log
+set log_comp=met\log_comp_%test_number%.log
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+%fme% met\metrique_NB_ORGANIZATION_MAPPER_featureResultat.fmw ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison data output with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM TEST 2: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - FEATURE OUTPUT
+SET source=met\source%test_number%.ffs
+set etalon=met\etalon%test_number%.ffs
+set lookup=met\OrganizationLookupTable.csv
+set resultat=met\resultat.ffs
+set log=met\log_%test_number%.log
+set log_comp=met\log_comp_%test_number%.log
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+
+%fme% met\metrique_NB_ORGANIZATION_MAPPER_featureResultat.fmw ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison data output with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM TEST 3: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - No Matching Organization Error OUTPUT
+set test_number=3
+SET source=met\source%test_number%.ffs
+set etalon=met\etalon%test_number%.ffs
+set lookup=met\OrganizationLookupTable.csv
+set resultat=met\resultat.ffs
+set log=met\log_%test_number%.log
+set log_comp=met\log_comp_%test_number%.log
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+
+%fme% met\metrique_NB_ORGANIZATION_MAPPER_errorResultatNoMatch.fmw ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison data output with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Test 4: 6 compliant records, 2 noncompliant with unmatched organization values, 1 with missing organization values. - Missing Organization Value Error OUTPUT
+set test_number=4
+SET source=met\source%test_number%.ffs
+set etalon=met\etalon%test_number%.ffs
+set lookup=met\OrganizationLookupTable.csv
+set resultat=met\resultat.ffs
+set log=met\log_%test_number%.log
+set log_comp=met\log_comp_%test_number%.log
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+
+%fme% met\metrique_NB_ORGANIZATION_MAPPER_errorResultatNoValue.fmw ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison data output with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM FINAL TEST
+@IF [%Statut%] EQU [000000] (
+ @ECHO INFORMATION : Metric test passed
+ @COLOR A0
+ @SET CodeSortie=999999
+) ELSE (
+ @ECHO ERROR: Metric test failed
+ @COLOR CF
+ @SET CodeSortie=-1
+)
+
+REM ===========================================================================
+REM We return the window to the starting directory
+REM ===========================================================================
+POPD
+ 
+REM ===========================================================================
+REM We pause so that the window does not close 
+REM in case we have to double-click on the.bat to execute it.
+REM ===========================================================================
+PAUSE
+COLOR
+EXIT /B %CodeSortie%
+
