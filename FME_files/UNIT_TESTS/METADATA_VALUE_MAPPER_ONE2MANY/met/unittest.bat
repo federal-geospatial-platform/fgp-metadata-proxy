16580dd2e9d8b858280dac213c3179e559971f33
robbennett62 <robbennett@rocketmail.com>
2021-03-08 10:05:50 -0500

    New unit test METADATA_VALUE_MAPPER_ONE2MANY

---
+++ FME_files/UNIT_TESTS/METADATA_VALUE_MAPPER_ONE2MANY/met/unittest.bat
@@ -0,0 +1,105 @@
+REM ===========================================================================
+REM Enable local variables 
+REM ===========================================================================
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+ 
+REM ===========================================================================
+REM Allow accented characters
+REM ===========================================================================
+chcp 1252
+
+REM ===========================================================================
+REM Determine the directory where the.bat is located and place it  
+REM in the directory above while keeping the original directory
+REM ===========================================================================
+SET Repertoire=%~dp0
+SET Ctfolder=%Repertoire%\..\..\..\FME_Custom_Transformers\
+PUSHD %Ctfolder%
+
+REM Define FME transformer path
+SET FME_USER_RESOURCE_DIR=%USERPROFILE%\Documents\FME
+
+REM ===========================================================================
+REM Create file name variable in relative mode.
+REM ===========================================================================
+SET NomApp=METADATA_VALUE_MAPPER_ONE2MANY
+SET fme=%FME2019%
+
+
+SET UserProfileFmx="%FME_USER_RESOURCE_DIR%\Transformers\%NomApp%.fmx"
+
+REM ===========================================================================
+REM Initialization of the variable that contains the result of the execution
+REM ===========================================================================
+SET Statut=0
+
+REM ===========================================================================
+REM Copy FMX to Documents
+REM ===========================================================================
+COPY/Y %Ctfolder%\%NomApp%.fmx %UserProfileFmx%
+SET Statut=%Statut%%ERRORLEVEL%
+
+PUSHD %Repertoire%\..
+
+REM Define sources
+
+REM First FME call,creating FFS File with eleven compliant data records, mapping projection codespace values
+SET test_number=1
+SET source=met\source%test_number%.ffs
+SET etalon=met\etalon%test_number%.ffs
+SET lookup=met\Subject_TBS.csv
+SET resultat=met\resultat.ffs
+SET log=met\log_%test_number%.log
+SET log_comp=met\log_comp_%test_number%.log
+SET att_to_map=TBS_Subject_temp{}.display_name
+SET code_refresh=NO
+SET english_refresh=YES
+SET error_not_mapped=YES
+SET french_refresh=NO
+
+IF EXIST %log% del %log%
+IF EXIST met\resultat.ffs DEL met\resultat.ffs
+%fme% met\metrique_metadata_value_mapper_one2many.fmw ^
+--ATT_TO_MAP %att_to_map% ^
+--CODE_REFRESH %code_refresh% ^
+--ENGLISH_REFRESH %english_refresh% ^
+--ERROR_NOT_MAPPED %error_not_mapped% ^
+--FRENCH_REFRESH %french_refresh% ^
+--IN_FFS_FILE %source% ^
+--OUT_FFS_FILE %resultat% ^
+--LOOKUP_TABLE %lookup% ^
+--LOG_FILE %log% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+REM Comparison data output with the standard
+IF EXIST %log_comp% del %log_comp%
+%fme% met\Comparateur.fmw ^
+--IN_ETALON_FILE %etalon% ^
+--IN_RESULTAT_FILE %resultat% ^
+--LOG_FILE %log_comp% 
+SET Statut=%Statut%%ERRORLEVEL%
+
+@IF [%Statut%] EQU [0000] (
+ @ECHO INFORMATION : Metric test passed
+ @COLOR A0
+ @SET CodeSortie=999999
+) ELSE (
+ @ECHO ERROR: Metric test failed
+ @COLOR CF
+ @SET CodeSortie=-1
+)
+
+REM ===========================================================================
+REM We return the window to the starting directory
+REM ===========================================================================
+POPD
+ 
+REM ===========================================================================
+REM We pause so that the window does not close 
+REM in case we have to double-click on the.bat to execute it.
+REM ===========================================================================
+PAUSE
+COLOR
+EXIT /B %CodeSortie%
+
