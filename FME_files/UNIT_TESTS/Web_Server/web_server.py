6007bce230523aee95cb1540dc8ab1f83c16546f
Dan-Eli <bergeronpilon@gmail.com>
2022-05-26 16:48:13 -0400

    Mise à jour du WebServer pour les tests unitaires

--- FME_files/UNIT_TESTS/Web_Server/web_server.py
+++ FME_files/UNIT_TESTS/Web_Server/web_server.py
@@ -33,6 +33,42 @@ def secondary_server():
 
     return resp
 
+@app.route("/nl_geo_primary")
+def nl_geo_primary_server():
+    with open ('./data/NL/nl_geo_primary.html', encoding="utf8") as html_input:
+        html_str = html_input.read()
+    resp = Response(response=html_str,
+                    status=200,
+                    mimetype="text/html")
+
+    return resp
+    
+@app.route("/nl_non_geo_primary")
+def nl_non_geo_primary_server():
+    with open ('./data/NL/nl_non_geo_primary.html', encoding="utf8") as html_input:
+        html_str = html_input.read()
+    resp = Response(response=html_str,
+                    status=200,
+                    mimetype="text/html")
+
+    return resp
+    
+@app.route("/public/opendata/page/")
+def nl_id_84():
+    args = request.args
+    if args["id"] in ["84", "741"]:
+        file_name = './data/NL/nl_id_84.html'
+    elif args["id"] in ["222", "421"]:
+        file_name = './data/NL/nl_id_222.html'
+    else:
+        raise Exception ("invalid dataset id: {0}".format(args["id"]))
+    with open (file_name, encoding="utf8") as html_input:
+        html_str = html_input.read()
+    resp = Response(response=html_str,
+                    status=200,
+                    mimetype="text/html")
+    
+    return resp
     
 @app.route("/testing")
 def testView1():
