76a7cbb7319e0ab0908598b5b4eb511d6282148e
nicgariepy <49950519+nicgariepy@users.noreply.github.com>
2024-02-05 15:36:50 -0500

    Ajustement DeltaFinderGen
    Ajustement DeltaFinderGen

--- FME_files/FME_Custom_Transformers/Python/METADATA_DELTA_FINDER_GEN.py
+++ FME_files/FME_Custom_Transformers/Python/METADATA_DELTA_FINDER_GEN.py
@@ -1,3 +1,4 @@
+#from Python.METADATA_DELTA_FINDER_GEN import FeatureProcessor
 import fme
 import fmeobjects
 import yaml
@@ -21,26 +22,29 @@ class FeatureProcessor(object):
         self.mapping = {}   #dictionnary containing the yaml content new and ref attributes to compare and their corresponding attribute names
         self.DictFeatureNew={}
         self.DictFeatureRef={}
-        
-        
-        
-    
-    
+
     def input(self, feature):
+        
+        #Chargement du fichier yaml en config. Si non chargé
         if not self.mapping:
             self.mapping = FME_utils.load_yaml_document(open(feature.getAttribute('_yaml_file'), "r").read())
             
             if 'id' not in self.mapping.keys():
-                raise
+                #C'est une erreur
+                feature.setAttribute('error',r"Il n'y a pas d'ID de défini dans le yaml")
+                self.pyoutput(feature)
         
+        #Extraction du type de source (new ou ref)
         source_type=feature.getAttribute('_source')
         
+        #Création des dictionnaires dans lesquels ont va ajouter  les valeurs hashées et brute pour la comparaison
         feature_dict_hash={}
+        feature_dict_value={}
         
         #Parcourir le yaml pour créer les hash
         for keys in self.mapping.keys():
             
-            #Extraction du nom générique de l'attribut
+            #Extraction du nom générique de l'attribut servant à décrire ce qu'on comparre 
             nom_eva=self.mapping[keys]['attr_name']
             
             
@@ -49,50 +53,148 @@ class FeatureProcessor(object):
                 nom_att=self.mapping[keys]['attr_ref']
             else:
                 nom_att=self.mapping[keys]['attr_new']
-                               
             
-            value=feature.getAttribute(nom_att)
+           
+            # Si un | est présent dans le nom de l'attribut (uniquement dans du new), cela
+            # veut dire qu'on doit concatener N attributs pour avoir la même valeur que dans la référence.
+            if '|' in nom_att:
+                #Extraire le séparateur utilisé pour concatener
+                sep=self.mapping[keys]['separator']
+                
+                #Vérifier si on concatène des attributs réguliers ou des listes
+                #Lorsque c'est une liste, chaque élément de la concaténation doit être une liste
+                #et toute les listes doivent être de la même longueur.
+                if '{}' in nom_att:
+                    
+                    #Gestion des attributs selon les listes
+                    #Création d'un dictionnaire pour stoker chaque liste
+                    dict_cont_list={}
+                    
+                    #Variable conservant la longueur de la/les listes
+                    global_len=0
+                    
+                    
+                    index=0
+                    #Créer un dictionnaire avec les listes de valeurs pour chaque atts
+                    for atts in nom_att.split('|'):
+                        #Vérifier que chaque sous-item est une liste
+                        if '{}' not in atts:
+                            
+                            feature.setAttribute('error',r"On ne peut concatener une liste  avec des attributs réguliers. L'attribut %s du yaml est en erreur"%(nom_eva))
+                            self.pyoutput(feature) 
+                            
+                        else:
+                            #Extraire la longueur de chaque liste et vérifier qu'elle est la même pour chaque liste
+                            if global_len==0:
+                                global_len=len(feature.getAttribute(atts))
+                            elif global_len != len(feature.getAttribute(atts)):
+                                    feature.setAttribute('error',r"Les listes concaténées doivent être de même longueur.L'attribut %s du yaml est en erreur"%(nom_eva))
+                                    self.pyoutput(feature) 
+                                    
+                            
+                            
+                            dict_cont_list[index]=feature.getAttribute(atts)
+                            #Incrémenter le compteur
+                            index+=1
+                   
+                    #Définir la variable Value (liste) dans laquel on viendra storer le résultat de la concaténation de chaque item du dictionnaire, groupé par position de liste similaire
+                    # E.G. ('A1,B1,C1')('A2,B2,C2')....
+                    value=[]
+                    
+                    # Boucler pour chaque index de liste
+                    for i in range(global_len):
+                        temp=''
+                        #Concaténer pour chaque dans l'ordre 1,2,3....
+                        
+                            
+                        for keys in list(dict_cont_list.keys()):
+                            if temp:
+                                temp='%s%s%s'%(temp,sep,dict_cont_list[keys][i])
+                            else:
+                                temp=dict_cont_list[keys][i]
+                        
+                        value.append(temp)
+                      
+                
+                #Lorsqu'il n'y a pas de liste dans la concaténation
+                else:
+                
+                    value=''
+                    #Parcourir chaque attribut, extraction de la valeur et concaténation avec l'espaceur choisi
+                    for atts in nom_att.split('|'):
+                                       
+                        temp=feature.getAttribute(atts)
+                        if value:
+                            value='%s%s%s'%(value,sep,temp)
+                        else:
+                            value=temp
+                            
+             
+            #À voir si on peut remplacer.           
+            #value=value.replace('<space>',' ')
             
             
             
+            else:
+                value=feature.getAttribute(nom_att)
             
+            
+            
+
+            #Définition de la variable id si l'attribut validé à l'ID
             if nom_eva=='id':
                 id=value
             
+            #Si absence de valeur, on défini value à une constante
             if value:
                 pass
             else:
                 value='METADATA_DELTA_FINDER_NO_VALUE'
 
+            #Si c'est une liste, on concatène le tout sans espaceur.
             if isinstance(value, list):
-                
+
                 
                 listToStr = ''.join([str(elem) for i,elem in enumerate(value)])
                 
                 value=listToStr
-                
-                
+            
+                      
             
             
-            
+            #Création d'une Hashing key à partir de la valeur extraite            
             value_hash=hashlib.sha256(value.encode('utf-8')).hexdigest()
             
                 
-
+            #Stockage de la valeur brute et de la clé de hashage pour attribut comparé dans les dictionnaires respectifs
+            #Dictionnaire de hashage
             feature_dict_hash[nom_eva]=value_hash
+            #Dictionnaire de valeur brute
+            feature_dict_value[nom_eva]=value
             
+        #Transfère en attribut des dictionnaires (hash et brute) sur la feature pour utilisation dans le close
         feature.setAttribute('feature_dict_hash',str(feature_dict_hash))
+        feature.setAttribute('feature_dict_value',str(feature_dict_value))
         
+        
+        #Transfère des features dans les dictionnaires de feature (new ou ref), la clé étant l'identifiant
         if source_type=='ref':
             self.DictFeatureRef[id]=feature
         else:
             self.DictFeatureNew[id]=feature
+  
+        
+        
         
     def close(self):
-        
-        
-        
-        #Boucler à partir des nouvelles métadonnées pour identifier les insert / update(ou no change)
+         
+            
+            
+            
+        #Parcourir le dictionnaires des nouvelles métadonnées pour identifier les insert / update(ou no change) 
+        #en comparant les clés du dictionnaire des nouvelles features avec celles de référence. Si la clé new n'est pas dans 
+        #dans le dictionnaire de référence, c'est un INSERT. Quand on a un match de clés, on doit comparer le contenu pour identifier 
+        #si c'est un no change ou un update.
         for nf_keys in self.DictFeatureNew.keys():
             
             #Extraction de la feature (new)  contenue dans le dictionnaire des nouvelles métadonnées
@@ -102,30 +204,53 @@ class FeatureProcessor(object):
             #Déterminer si l'identifiant (la feature) existe dans la métadonnées référence
             if nf_keys in self.DictFeatureRef.keys():
                 
+                # On a un match entre le new et le ref. On doit comparer les clés de hashage pour déterminé Update/No Chant
                 
                 feature_ref=self.DictFeatureRef[nf_keys]
                 
-                #Hash global new
+                #Extraction de la clé de hashage de la nouvelle feature
                 hash_detail_new=feature_new.getAttribute('feature_dict_hash')
-                #Hash global ref
+                #Extraction de la clé de hashage de la feature de référence
                 hash_detail_ref=feature_ref.getAttribute('feature_dict_hash')
                 
                 
                 if hash_detail_new != hash_detail_ref:
-                    # Faire ressortir la feature new avec la transaction Update ainsi que les 2 hash pour trouver la différence
+                    #Les clés de hashahge sont différentes, on est en mode Update
+                    
+                    #Supprimer l'attribut contenant le clé de hashage sur la nouvelle feature. Attribut non utile 
+                    #à cette étape du traitement 
                     feature_new.removeAttribute('feature_dict_hash')
+                    #Définition du PyCSW Transaction à update
                     feature_new.setAttribute('PyCSW_Transaction','Update')
-                    feature_new.setAttribute('_hash_detail_new',hash_detail_new)
-                    feature_new.setAttribute('_hash_detail_ref',hash_detail_ref)
                     
+                    #Comparer en détail les clés de hashage pour identifier les différences
+                    new = set(eval(hash_detail_new).items())
+                    ref = set(eval(hash_detail_ref).items())
+                    diff_hash=new ^ ref
+      
+                    #Extraction les dictionnaires de valeurs brutes pour identifier les valeurs d'origine pour chaque keys
+                    dict_value_new=eval(feature_new.getAttribute('feature_dict_value'))
+                    dict_value_ref=eval(feature_ref.getAttribute('feature_dict_value'))
+                
                     
-                    set1 = set(eval(hash_detail_new).items())
+                    #Parcourrir la listes de différence et extraire les valeurs new et ref correspondant aux attributs 
                     
-                    
-                    set2 = set(eval(hash_detail_ref).items())
-                    diff_hash=set1 ^ set2
-                    
-                    feature_new.setAttribute('_diff_hash',str(diff_hash))
+                    #Liste servant à stoker les différences
+                    att_diff={}
+                    for items in diff_hash:
+                        
+                        #uniquement si l'item n'est pas dans le dictionnaire
+                        if items[0] not in att_diff.keys():
+                            
+                            #Création d'un dictionnaire temporaire pour stoker les valeurs new et ref associées à l'attribut
+                            dict_temp={}
+                            dict_temp['new']=dict_value_new[items[0]]
+                            dict_temp['ref']=dict_value_ref[items[0]]
+                            #Ajout dans le dictionnaire des différences le dictionnaire temporaire
+                            att_diff[items[0]]=dict_temp
+                            
+                    #Transfert en attribut sur la feature du dictionnaire des différences
+                    feature_new.setAttribute('_diff_detil',str(att_diff))
                     
                     
                 else:
@@ -139,7 +264,7 @@ class FeatureProcessor(object):
             
             #On fait ressortir la nouvelle feature avec la bonne transaction
             self.pyoutput(feature_new)
-        
+   
 
         #On boucle à l'inverse dans les références pour identifier les feature de référence n'étant plus dans les new
         for rf_keys in self.DictFeatureRef.keys():
@@ -151,4 +276,4 @@ class FeatureProcessor(object):
                 feature_del.setAttribute('PyCSW_Transaction','Delete')
                 
                 self.pyoutput(feature_del)
-                
+               
