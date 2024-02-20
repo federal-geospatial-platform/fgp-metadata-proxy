2196ed710018996a44f69ae1a44d31f5aa3fbd81
Dan-Eli <bergeronpilon@gmail.com>
2024-02-20 16:16:00 -0500

    Update MV_CATALOGUE_READER.py
    First version OK to read the MV web site

--- FME_files/FME_Custom_Transformers/Python/MV_CATALOGUE_READER.py
+++ FME_files/FME_Custom_Transformers/Python/MV_CATALOGUE_READER.py
@@ -19,69 +19,16 @@ ATT_URL_DATASET_GEO = "_url_dataset_geo"
 ATT_URL_DATASET_NON_GEO = "_url_dataset_non_geo"
 ATT_URL_ERR = "_url_error"
 ATT_URL_ROOT = "_url_root"
-ATT_URL_SECONDARY_1 = "_html_p_div{}.a.href"
-ATT_URL_SECONDARY_2 = "_html_p_div{}.br.a.href"
 ATT_HTML_GEO_FULL = "_html_geo_full"
-ATT_HTML_NON_GEO_FULL = "_html_non_geo_full"
-ATT_HTML_DETAIL = "_html_detail"
-ATT_URL_METADATA = "url_metadata"
 
 
-
-def catalogue_reader_primary(feature):
-    """This method reads the pimary catalogue of Newfoundland and Labrador
-    
-    Parameters
-    ----------
-    feature : FME Feature
-        FME feature to process
-    
-    Returns
-    -------
-    None
-    """
-    
-    web_pdb.set_trace()
-    # Read the geo datasets
-    url_dataset_geo = FME_utils.feature_get_attribute(feature, ATT_URL_DATASET_GEO)
-    if url_dataset_geo != "":
-        # Read the url
-
-        try:
-            xhtml = FME_utils.extract_url_html(url_dataset_geo)
-
-            soup = BeautifulSoup(xhtml, "html.parser")
-            xhtml_lst = soup.find_all('tbody')
-            for xhtml in xhtml_lst:
-                xhtml_str = xhtml.prettify()
-                cloned_feature = feature.clone()
-                cloned_feature.setAttribute(ATT_HTML_GEO_FULL, xhtml_str)
-                self.pyoutput.cloned_feature
-
-        except:
-            feature.setAttribute(ATT_URL_ERR, url_dataset_geo)
-            
-#    # Read the non geo datasets
-#    url_dataset_non_geo = FME_utils.feature_get_attribute(feature, ATT_URL_DATASET_NON_GEO)
-#    if url_dataset_non_geo != "":
-#        # Read the url
-#        try:
-#            xhtml = FME_utils.extract_url_html(url_dataset_non_geo)
-#            feature.setAttribute(ATT_HTML_NON_GEO_FULL, xhtml)
-#        except:
-#            feature.setAttribute(ATT_URL_ERR, url_dataset_non_geo)
-        
-    return
-    
 class CatalogueReaderMacValley(object):
     """This class reads the html code of each datasets.
     
-    Reading of the NL datasets.
+    Reading of the Mavckenzy Valley datasets.
     
-    For NL there is no API to extract the information in JSON and/or XML like format.
+    For Mackenzy Valley there is no API to extract the information in JSON and/or XML like format.
     So we must read all the HTML code and perform HTML tag extraction to get the metadata.
-    The *Requests* is not working properly so we implemented the standard library urllib.request
-    to read the HTML code (used in the method FME_utils.extract_url_html)
     
     """
 
@@ -116,7 +63,8 @@ class CatalogueReaderMacValley(object):
         self.feature = feature
         
     def close(self):
-        """Read  the HTML code for each dataset.
+        """Read  the HTML code for the MV web site. Break into different FME feature for <tbody> tag
+        in the XML dcument.
         
         Parameters
         ----------
@@ -139,6 +87,7 @@ class CatalogueReaderMacValley(object):
                 soup = BeautifulSoup(xhtml, "html.parser")
                 xhtml_lst = soup.find_all('tbody')
                 for xhtml in xhtml_lst:
+                    # Create a FME feature for each <tbody> tag.
                     xhtml_str = xhtml.prettify()
                     cloned_feature = self.feature.clone()
                     cloned_feature.setAttribute(ATT_HTML_GEO_FULL, xhtml_str)
@@ -173,6 +122,8 @@ class MackenzieValleySecondary(object):
         """
 
         def _del_attributes():
+            """This routine deletes all the attributes that starts with a predefined prefix."""
+
             for attrib in feature_cloned.getAllAttributeNames():
                 if attrib.startswith('_html_p_'):
                     feature_cloned.removeAttribute(attrib)
@@ -198,6 +149,7 @@ class MackenzieValleySecondary(object):
 
                 _del_attributes()
 
+
                 url_mv = feature_cloned.getAttribute('_url_root')
                 url_detail = url_mv + url_suffix
 
@@ -206,81 +158,197 @@ class MackenzieValleySecondary(object):
 
                 soup = BeautifulSoup(xhtml, "html.parser")
                 xhtml_lst = soup.find_all('tbody')
-                logger = fmeobjects.FMELogFile()
                 try:
                     feature_cloned.setAttribute('_html_desc', xhtml_lst[0].prettify())
                 except:
                     logger.logMessageString("No details found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
 
-                try:
-                    feature_cloned.setAttribute('_html_ref1', xhtml_lst[1].prettify())
-                except:
-                    logger.logMessageString("No references found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
 
-                try:
-                    feature_cloned.setAttribute('_html_ref2', xhtml_lst[2].prettify())
-                except:
-                    logger.logMessageString("No references found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
-
+                # web_pdb.set_trace()
                 xhtml_lst = soup.find_all(attrs = {"class": "field field-name-field-body field-type-text-long field-label-hidden clearfix"})
                 try:
-                    feature_cloned.setAttribute('_html_def', xhtml_lst[0].prettify())
+                    description = str(xhtml_lst[0].p.string)
+                    feature_cloned.setAttribute("mv_description", description)
                 except:
                     logger.logMessageString("No description found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
+                    feature_cloned.setAttribute("mv_description", "")
+
+                pos = xhtml.find("Total Documents (")
+                pos_start = xhtml[pos:].find('"')
+                pos_end = xhtml[pos+pos_start+1:].find('"')
+                if pos != -1 and pos_start != -1 and pos_end != -1:
+                    # Extract the list of document
+                    try:
+                        txt_start = pos+pos_start+1
+                        url_doc_suffix = xhtml[txt_start:txt_start+pos_end]
+                        url_doc_all = url_mv + url_doc_suffix
+                        xhtml = FME_utils.extract_url_html(url_doc_all)
+
+                        soup = BeautifulSoup(xhtml, "html.parser")
+                        soup_lst = soup.find_all(attrs={"class": "views-row"})
+
+                        i = 0
+                        for item in soup_lst:
+
+                            # web_pdb.set_trace()
+                            titre = str(item.h3.a.string)
+                            url_doc = str(item.h3.a['href'])
+
+                            items = item.find_all(attrs={"class": "views-field views-field-file-size"})
+                            doc_size = str(items[0].span.string)
+
+                            items = item.find_all(attrs={"class": "views-field views-field-date-received"})
+                            sub_items = items[0].find_all(attrs={"class": "field-content"})
+                            date_recu = str(sub_items[0].string)
+
+                            items = item.find_all(attrs={"class": "views-field views-field-document-date"})
+                            sub_items = items[0].find_all(attrs={"class": "field-content"})
+                            date_doc = str(sub_items[0].string)
+
+                            items = item.find_all(attrs={"class": "views-field views-field-originator"})
+                            sub_items = items[0].find_all(attrs={"class": "field-content"})
+                            originator = str(sub_items[0].string)
+
+                            # web_pdb.set_trace()
+                            items = item.find_all(attrs={"class": "views-field views-field-document-stage-name"})
+                            sub_items = items[0].find_all(attrs={"class": "field-content"})
+                            doc_name = str(sub_items[0].string)
+
+                            feature_cloned.setAttribute('mv_ref{%i}.titre' %i, titre)
+                            feature_cloned.setAttribute('mv_ref{%i}.url' %i, url_mv + url_doc)
+                            feature_cloned.setAttribute('mv_ref{%i}.doc_size' %i, doc_size)
+                            feature_cloned.setAttribute('mv_ref{%i}.date_recu' %i, date_recu)
+                            feature_cloned.setAttribute('mv_ref{%i}.date_doc' %i, date_doc)
+                            feature_cloned.setAttribute('mv_ref{%i}.originator' %i, originator)
+                            feature_cloned.setAttribute('mv_ref{%i}.doc_name' %i, doc_name)
+                            i += 1
+
+                    except:
+                        logger.logMessageString("Unable to extract the  content of the URL {0}".format(url_doc_suffix),
+                                                fmeobjects.FME_WARN)
+                else:
+                    # Unable to locate the URL suffix of the documentation
+                    logger.logMessageString("Unable to find the URL of the documents {0}".format(url_doc_suffix),
+                                            fmeobjects.FME_WARN)
+
+
+                # Extract the project date range
+                # web_pdb.set_trace()
+                try:
+                    url_date = url_mv + url_suffix + "/timeline"
+                    xhtml = FME_utils.extract_url_html(url_date)
+                    feature_cloned.setAttribute('_html_timeline', xhtml)
+
+                    soup = BeautifulSoup(xhtml, "html.parser")
+                    start_lst = soup.find_all(attrs={"class": "date-display-start"})
+                    end_lst = soup.find_all(attrs={"class": "date-display-end"})
+                    try:
+                        feature_cloned.setAttribute("mv_date_start", str(start_lst[0].string))
+                        feature_cloned.setAttribute('mv_date_end', str(end_lst[0].string))
+                    except:
+                        logger.logMessageString("No date range found in HTML code in URL {0}".format(url_date),
+                                                fmeobjects.FME_WARN)
+                except:
+                    feature_cloned.setAttribute('_html_timeline', "<empty>")
 
                 self.pyoutput(feature_cloned)
 
 def MackenzieValleyDetail(feature):
+    """This routine sets different FME attributes from the flatten XML documents.
+
+        Parameters
+        ----------
+        feature : FME Feature
+                The feature to process
+
+        Returns:
+        --------
+        None
+        """
+
+    def _set_min_max_date():
+        # If nodate are present this module extract the min max date from the date in the references
+
+        # Check if start date and end date exist
+        date_start = feature.getAttribute("mv_date_start")
+        date_end = feature.getAttribute('mv_date_end')
+
+        if date_start is None and date_end is None:
+            # The start and end dates do not exist
+            ind = FME_utils.max_index_attribute_list(feature, "mv_ref{}.date_doc")
+            date_lst = []
+            # Loop over the reference to extract the dates
+            for i in range(ind):
+                date_doc = feature.getAttribute("mv_ref{%i}.date_doc" % i)
+                date_doc = date_doc.replace("\n", "")  # remove all \n (return)
+                date_doc = date_doc.strip()  # Remove starting and ending whitespaces
+
+                try:
+                    date = datetime.strptime(date_doc, "%b %d, %Y")  # Convert string date to object date
+                    date_lst.append(date)
+                except:
+                    pass
+
+            if len(date_lst) >= 1:
+                # Extract the min date, max date and set the attributes
+                date_start = min(date_lst)
+                date_end = max(date_lst)
+                feature.setAttribute("mv_date_start", date_start.strftime("%b %d, %Y"))
+                feature.setAttribute("mv_date_end", date_end.strftime("%b %d, %Y"))
+
+    def _set_lat_lon():
+        # Set the latitute and longitude
+
+        html = feature.getAttribute("_html_detail")  # Get the flattend xml code
+        try:
+            lat_lon_start = html.find("deltas")
+            lat_lon_end = html[lat_lon_start:].find("}") + lat_lon_start
+            lat_lon_str = '{"' + html[lat_lon_start:lat_lon_end] + "}}}"
+            lat_lon_dict = eval(lat_lon_str)  # Transform the string into a dictionary
+            lat = float(lat_lon_dict["deltas"]["d_0"]["lat"])  # extract the lat
+            lon = float(lat_lon_dict["deltas"]["d_0"]["lng"])  # Extract the lon
+        except:
+            # Any error during lat/lon extraction will reset the lat/lon to 0.
+            lat = 0.
+            lon = 0.
+
+        # Set the FME attributes
+        feature.setAttribute("mv_lat", lat)
+        feature.setAttribute("mv_lon", lon)
+
+    def _set_contact_info():
+        # Extract different information from the flatten XML code
+
+        for i in range(4):
+            code = feature.getAttribute("_html_pd_tr{%i}.th" % i)
+            if code is None:
+                break
+            if "type" in code.lower():
+                type = feature.getAttribute("_html_pd_tr{%i}.td" % i)
+                feature.setAttribute('mv_type', type)
+            elif "status" in code.lower():
+                status = feature.getAttribute("_html_pd_tr{%i}.td" % i)
+                feature.setAttribute('mv_status', status)
+            elif "contact" in code.lower():
+                contact = feature.getAttribute("_html_pd_tr{%i}.td.a" % i)
+                email_str = feature.getAttribute("_html_pd_tr{%i}.td.a.href" % i)
+                email_pattern = re.compile(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})')
+                email_match = re.findall(email_pattern, email_str)
+                feature.setAttribute('mv_contact', contact)
+                feature.setAttribute('mv_email', email_match[0])
+            else:
+                pass
 
     logger = fmeobjects.FMELogFile()
 
     # web_pdb.set_trace()
-    for i in range(4):
-        code = feature.getAttribute("_html_pd_tr{%i}.th" %i)
-        if code is None:
-            break
-        if "type" in code.lower():
-            type = feature.getAttribute("_html_pd_tr{%i}.td" %i)
-            feature.setAttribute('type', type)
-        elif "status" in code.lower():
-            status = feature.getAttribute("_html_pd_tr{%i}.td" %i)
-            feature.setAttribute('status', status)
-        elif "contact" in code.lower():
-            contact = feature.getAttribute("_html_pd_tr{%i}.td.a" %i)
-            email_str = feature.getAttribute("_html_pd_tr{%i}.td.a.href" %i)
-            email_pattern = re.compile(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})')
-            email_match = re.findall(email_pattern, email_str)
-            feature.setAttribute('contact', contact)
-            feature.setAttribute('email', email_match[0])
-        else:
-            pass
+    # Set contact information
+    _set_contact_info()
 
-    # web_pdb.set_trace()
-    url_root = feature.getAttribute(ATT_URL_ROOT)
-    ind = 0
-    for html_prefix in ("_html_ref1_", "_html_ref2_"):
-        for i in range(9999):
-            att = html_prefix+"tr{%i}.td{0}.a" %i
-            title = feature.getAttribute(att)
-            title = feature.getAttribute (html_prefix+"tr{%i}.td{0}.a" %i)
-            link = feature.getAttribute (html_prefix+"tr{%i}.td{0}.a.href" %i)
-            date = feature.getAttribute (html_prefix+"tr{%i}.td{1}" %i)
-            if title is None and link is None and date is None:
-                break
-            if title is None: title = ""
-            if link is None:
-                title = ""
-            else:
-                link = url_root + link
-            if date is None: title = ""
-            feature.setAttribute("ref{%i}.title" %i, title)
-            feature.setAttribute("ref{%i}.link" %i, link)
-            feature.setAttribute("ref{%i}.date" %i, date)
-            i += 1
+    # Set the lat/lon
+    _set_lat_lon()
 
-    description = feature.getAttribute("_html_pdef_p")
-    if description is None:
-        description = ""
-    feature.setAttribute("description", description)
+    # Set the start end date (if missing)
+    _set_min_max_date()
 
     return
