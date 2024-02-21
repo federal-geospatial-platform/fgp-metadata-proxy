736fb8a0fb2623f727bd4780e6726aaf1126732f
Daniel Pilon <bergeronpilon@gmail.com>
2024-02-21 16:35:39 -0500

    Catalogue reader for Mackenzy Valley web site
    Containing a python reader and a FME desktop

--- FME_files/FME_Custom_Transformers/Python/MV_CATALOGUE_READER.py
+++ FME_files/FME_Custom_Transformers/Python/MV_CATALOGUE_READER.py
@@ -14,9 +14,7 @@ except:
 
 # Define FME attribute names
 
-ATT_URL_DETAIL = "_html_p_div{}.a.href"
 ATT_URL_DATASET_GEO = "_url_dataset_geo"
-ATT_URL_DATASET_NON_GEO = "_url_dataset_non_geo"
 ATT_URL_ERR = "_url_error"
 ATT_URL_ROOT = "_url_root"
 ATT_HTML_GEO_FULL = "_html_geo_full"
@@ -27,7 +25,7 @@ class CatalogueReaderMacValley(object):
     
     Reading of the Mavckenzy Valley datasets.
     
-    For Mackenzy Valley there is no API to extract the information in JSON and/or XML like format.
+    For Mackenzy Valley there is no API avalaible to extract the information in JSON or XML format.
     So we must read all the HTML code and perform HTML tag extraction to get the metadata.
     
     """
@@ -87,7 +85,7 @@ class CatalogueReaderMacValley(object):
                 soup = BeautifulSoup(xhtml, "html.parser")
                 xhtml_lst = soup.find_all('tbody')
                 for xhtml in xhtml_lst:
-                    # Create a FME feature for each <tbody> tag.
+                    # Create a different FME feature for each <tbody> tag.
                     xhtml_str = xhtml.prettify()
                     cloned_feature = self.feature.clone()
                     cloned_feature.setAttribute(ATT_HTML_GEO_FULL, xhtml_str)
@@ -121,135 +119,167 @@ class MackenzieValleySecondary(object):
         None
         """
 
-        def _del_attributes():
-            """This routine deletes all the attributes that starts with a predefined prefix."""
+        def _extract_timeline():
+            """Extract the project time range"""
 
-            for attrib in feature_cloned.getAllAttributeNames():
-                if attrib.startswith('_html_p_'):
-                    feature_cloned.removeAttribute(attrib)
+            try:
+                url_date = url_mv + url_suffix + "/timeline"
+                xhtml = FME_utils.extract_url_html(url_date)
+                feature_cloned.setAttribute('_html_timeline', xhtml)
 
-        # web_pdb.set_trace()
-        logger = fmeobjects.FMELogFile()
-        for feature in self.in_feature:
-            # Loop over the flatten XHTML document in order to extract specific tags into FME attribute
-            for ind in range(999):
-                company = feature.getAttribute("_html_p_tr{%i}.td{0}" % ind)
-                project = feature.getAttribute("_html_p_tr{%i}.td{1}.a" % ind)
-                code = feature.getAttribute("_html_p_tr{%i}.td{2}" % ind)
-                year = feature.getAttribute("_html_p_tr{%i}.td{3}.span" % ind)
-                url_suffix = feature.getAttribute("_html_p_tr{%i}.td{1}.a.href" % ind)
-                if company is None and project is None and code is None and year is None and url_suffix is None:
-                    break
-                feature_cloned = feature.clone()
+                soup = BeautifulSoup(xhtml, "html.parser")
+                start_lst = soup.find_all(attrs={"class": "date-display-start"})
+                end_lst = soup.find_all(attrs={"class": "date-display-end"})
+                try:
+                    feature_cloned.setAttribute("mv_date_start", str(start_lst[0].string))
+                    feature_cloned.setAttribute('mv_date_end', str(end_lst[0].string))
+                except:
+                    logger.logMessageString("No date range found in HTML code in URL {0}".format(url_date),
+                                            fmeobjects.FME_WARN)
+            except:
+                feature_cloned.setAttribute('_html_timeline', "<empty>")
+
+            return
+
+        def _extract_references():
+
+            pos = xhtml.find("Total Documents (")
+            pos_start = xhtml[pos:].find('"')
+            pos_end = xhtml[pos + pos_start + 1:].find('"')
+            if pos != -1 and pos_start != -1 and pos_end != -1:
+                # Extract the list of document
+                try:
+                    txt_start = pos + pos_start + 1
+                    url_doc_suffix = xhtml[txt_start:txt_start + pos_end]
+                    url_doc_all = url_mv + url_doc_suffix
+                    # Retrive the html documentation
+                    xhtml_doc = FME_utils.extract_url_html(url_doc_all)
+
+                    soup = BeautifulSoup(xhtml_doc, "html.parser")
+                    soup_lst = soup.find_all(attrs={"class": "views-row"})
+
+                    i = 0
+                    # Each list item represent one document
+                    for item in soup_lst:
+                        # Extract the information of one document
+                        titre = str(item.h3.a.string)
+                        url_doc = str(item.h3.a['href'])
+
+                        items = item.find_all(attrs={"class": "views-field views-field-file-size"})
+                        doc_size = str(items[0].span.string)
+
+                        items = item.find_all(attrs={"class": "views-field views-field-date-received"})
+                        sub_items = items[0].find_all(attrs={"class": "field-content"})
+                        date_recu = str(sub_items[0].string)
+
+                        items = item.find_all(attrs={"class": "views-field views-field-document-date"})
+                        sub_items = items[0].find_all(attrs={"class": "field-content"})
+                        date_doc = str(sub_items[0].string)
+
+                        items = item.find_all(attrs={"class": "views-field views-field-originator"})
+                        sub_items = items[0].find_all(attrs={"class": "field-content"})
+                        originator = str(sub_items[0].string)
+
+                        # web_pdb.set_trace()
+                        items = item.find_all(attrs={"class": "views-field views-field-document-stage-name"})
+                        sub_items = items[0].find_all(attrs={"class": "field-content"})
+                        doc_name = str(sub_items[0].string)
+
+                        # Set the FME attribute on the deature
+                        feature_cloned.setAttribute('mv_ref{%i}.titre' % i, titre)
+                        feature_cloned.setAttribute('mv_ref{%i}.url' % i, url_mv + url_doc)
+                        feature_cloned.setAttribute('mv_ref{%i}.doc_size' % i, doc_size)
+                        feature_cloned.setAttribute('mv_ref{%i}.date_recu' % i, date_recu)
+                        feature_cloned.setAttribute('mv_ref{%i}.date_doc' % i, date_doc)
+                        feature_cloned.setAttribute('mv_ref{%i}.originator' % i, originator)
+                        feature_cloned.setAttribute('mv_ref{%i}.doc_name' % i, doc_name)
+                        i += 1
+
+                except:
+                    logger.logMessageString("Unable to extract the  content of the URL {0}".format(url_doc_suffix),
+                                            fmeobjects.FME_WARN)
+            else:
+                # Unable to locate the URL suffix of the documentation
+                logger.logMessageString("Unable to find the URL of the documents {0}".format(url_doc_suffix),
+                                        fmeobjects.FME_WARN)
+
+            return
+
+        def _extract_description():
+            """Extract the project description"""
+
+            url_detail = url_mv + url_suffix
+            xhtml_detail = FME_utils.extract_url_html(url_detail)
+            feature_cloned.setAttribute('_html_detail', xhtml_detail)
+
+            soup = BeautifulSoup(xhtml_detail, "html.parser")
+            xhtml_lst = soup.find_all('tbody')
+            try:
+                feature_cloned.setAttribute('_html_desc', xhtml_lst[0].prettify())
+            except:
+                logger.logMessageString("No details found in HTML code in URL {0}".format(url_detail),
+                                        fmeobjects.FME_WARN)
+
+            # web_pdb.set_trace()
+            xhtml_lst = soup.find_all(
+                attrs={"class": "field field-name-field-body field-type-text-long field-label-hidden clearfix"})
+            try:
+                description = str(xhtml_lst[0].p.string)
+                feature_cloned.setAttribute("mv_description", description)
+            except:
+                logger.logMessageString("No description found in HTML code in URL {0}".format(url_detail),
+                                        fmeobjects.FME_WARN)
+                feature_cloned.setAttribute("mv_description", "")
+
+            return xhtml_detail
+
+        def _extract_info():
+            """Extract project information."""
+
+            company = feature.getAttribute("_html_p_tr{%i}.td{0}" % ind)
+            project = feature.getAttribute("_html_p_tr{%i}.td{1}.a" % ind)
+            code = feature.getAttribute("_html_p_tr{%i}.td{2}" % ind)
+            year = feature.getAttribute("_html_p_tr{%i}.td{3}.span" % ind)
+            if company is None and project is None and code is None and year is None and url_suffix is None:
+                # Nothing to extract
+                extracted = False
+            else:
+                extracted = True
                 feature_cloned.setAttribute('mv_company', company)
                 feature_cloned.setAttribute('mv_project', project)
                 feature_cloned.setAttribute('mv_code', code)
                 feature_cloned.setAttribute('mv_year', year)
                 feature_cloned.setAttribute('mv_url_suffix', url_suffix)
 
-                _del_attributes()
+            return extracted
 
+        # web_pdb.set_trace()
+        logger = fmeobjects.FMELogFile()
 
-                url_mv = feature_cloned.getAttribute('_url_root')
-                url_detail = url_mv + url_suffix
+        # Loop over each feature to extract exch specific MV project
+        for feature in self.in_feature:
+            # Loop over the flatten XHTML document in order to extract specific tags into FME attribute
+            for ind in range(999):
 
-                xhtml = FME_utils.extract_url_html(url_detail)
-                feature_cloned.setAttribute('_html_detail', xhtml)
+                url_suffix = feature.getAttribute("_html_p_tr{%i}.td{1}.a.href" % ind)
+                url_mv = feature.getAttribute('_url_root')
+                feature_cloned = feature.clone()
 
-                soup = BeautifulSoup(xhtml, "html.parser")
-                xhtml_lst = soup.find_all('tbody')
-                try:
-                    feature_cloned.setAttribute('_html_desc', xhtml_lst[0].prettify())
-                except:
-                    logger.logMessageString("No details found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
-
-
-                # web_pdb.set_trace()
-                xhtml_lst = soup.find_all(attrs = {"class": "field field-name-field-body field-type-text-long field-label-hidden clearfix"})
-                try:
-                    description = str(xhtml_lst[0].p.string)
-                    feature_cloned.setAttribute("mv_description", description)
-                except:
-                    logger.logMessageString("No description found in HTML code in URL {0}".format(url_detail), fmeobjects.FME_WARN)
-                    feature_cloned.setAttribute("mv_description", "")
-
-                pos = xhtml.find("Total Documents (")
-                pos_start = xhtml[pos:].find('"')
-                pos_end = xhtml[pos+pos_start+1:].find('"')
-                if pos != -1 and pos_start != -1 and pos_end != -1:
-                    # Extract the list of document
-                    try:
-                        txt_start = pos+pos_start+1
-                        url_doc_suffix = xhtml[txt_start:txt_start+pos_end]
-                        url_doc_all = url_mv + url_doc_suffix
-                        xhtml = FME_utils.extract_url_html(url_doc_all)
-
-                        soup = BeautifulSoup(xhtml, "html.parser")
-                        soup_lst = soup.find_all(attrs={"class": "views-row"})
-
-                        i = 0
-                        for item in soup_lst:
-
-                            # web_pdb.set_trace()
-                            titre = str(item.h3.a.string)
-                            url_doc = str(item.h3.a['href'])
-
-                            items = item.find_all(attrs={"class": "views-field views-field-file-size"})
-                            doc_size = str(items[0].span.string)
-
-                            items = item.find_all(attrs={"class": "views-field views-field-date-received"})
-                            sub_items = items[0].find_all(attrs={"class": "field-content"})
-                            date_recu = str(sub_items[0].string)
-
-                            items = item.find_all(attrs={"class": "views-field views-field-document-date"})
-                            sub_items = items[0].find_all(attrs={"class": "field-content"})
-                            date_doc = str(sub_items[0].string)
-
-                            items = item.find_all(attrs={"class": "views-field views-field-originator"})
-                            sub_items = items[0].find_all(attrs={"class": "field-content"})
-                            originator = str(sub_items[0].string)
-
-                            # web_pdb.set_trace()
-                            items = item.find_all(attrs={"class": "views-field views-field-document-stage-name"})
-                            sub_items = items[0].find_all(attrs={"class": "field-content"})
-                            doc_name = str(sub_items[0].string)
-
-                            feature_cloned.setAttribute('mv_ref{%i}.titre' %i, titre)
-                            feature_cloned.setAttribute('mv_ref{%i}.url' %i, url_mv + url_doc)
-                            feature_cloned.setAttribute('mv_ref{%i}.doc_size' %i, doc_size)
-                            feature_cloned.setAttribute('mv_ref{%i}.date_recu' %i, date_recu)
-                            feature_cloned.setAttribute('mv_ref{%i}.date_doc' %i, date_doc)
-                            feature_cloned.setAttribute('mv_ref{%i}.originator' %i, originator)
-                            feature_cloned.setAttribute('mv_ref{%i}.doc_name' %i, doc_name)
-                            i += 1
-
-                    except:
-                        logger.logMessageString("Unable to extract the  content of the URL {0}".format(url_doc_suffix),
-                                                fmeobjects.FME_WARN)
+                # Extract project general information
+                if _extract_info():
+                    pass
                 else:
-                    # Unable to locate the URL suffix of the documentation
-                    logger.logMessageString("Unable to find the URL of the documents {0}".format(url_doc_suffix),
-                                            fmeobjects.FME_WARN)
+                    # Extraction finished
+                    break
 
+                # Extract the project description
+                xhtml = _extract_description()
+
+                # Extract the project references
+                _extract_references()
 
                 # Extract the project date range
-                # web_pdb.set_trace()
-                try:
-                    url_date = url_mv + url_suffix + "/timeline"
-                    xhtml = FME_utils.extract_url_html(url_date)
-                    feature_cloned.setAttribute('_html_timeline', xhtml)
-
-                    soup = BeautifulSoup(xhtml, "html.parser")
-                    start_lst = soup.find_all(attrs={"class": "date-display-start"})
-                    end_lst = soup.find_all(attrs={"class": "date-display-end"})
-                    try:
-                        feature_cloned.setAttribute("mv_date_start", str(start_lst[0].string))
-                        feature_cloned.setAttribute('mv_date_end', str(end_lst[0].string))
-                    except:
-                        logger.logMessageString("No date range found in HTML code in URL {0}".format(url_date),
-                                                fmeobjects.FME_WARN)
-                except:
-                    feature_cloned.setAttribute('_html_timeline', "<empty>")
+                _extract_timeline()
 
                 self.pyoutput(feature_cloned)
 
